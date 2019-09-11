0d9be745769e801b3a87d47d172c91a76cf323a2
Tawakkal23 <55172998+Tawakkal23@users.noreply.github.com>
2019-09-11 12:47:50 +0800

    Update REWA.sh

--- REWA.sh
+++ REWA.sh
@@ -10,7 +10,9 @@ echo
 read -p "SIAPA NAMATA' :"  NAMA
 echo
 echo "DUDUK MAKI' DULU $NAMA KARNA MASIH LOADING" | lolcat
-echo "sedang menginstall RED_HAWK...."
+echo
+echo "sedang menginstall RED_HAWK...." | lolcat
+
 sleep 10
 
 
@@ -19,9 +21,10 @@ pkg install git
 pkg install php
 
 git clone https://github.com/Tuhinshubhra/RED_HAWK
-echo "${y} > Installer RED_HAWK..."
-echo "${y} > cd RED_HAWK"
-echo "${y} > php RED_HAWK.php"
-cd /data/data/com.termux/files/home/RED_HAWK/
-php /data/data/com.termux/files/home/RED_HAWK/ RED_HAWK.php
+cd RED_HAWK
+php rhwak.php
+
+
+
+
 
